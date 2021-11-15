53383f91329493eb3abb91164e9d5329825ddb14
angotron <92089667+angotron@users.noreply.github.com>
2021-11-15 19:23:46 +0100

    added api icon

--- src/script.js
+++ src/script.js
@@ -30,23 +30,21 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind-speed");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#main-icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+let city = "New York";
 let apiKey = "8050aaa898a67ab7de90e5b1200fb4e2";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=${apiKey}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 axios.get(apiUrl).then(displayTemperature);
-
-/* 
-function displayIcon(response) {
-  let iconElement = document.querySelector("#main-icon");
-  iconElement.innerHTML = response.data.weather[0].icon;
-}
-let apiIconUrl = `http://openweathermap.org/img/wn/10d@2x.png`;
-axios.get(apiIconUrl).then(displayIcon);
-
-*/
